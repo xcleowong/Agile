224c875201c403d58d080de06b1e6a75cc7a40c2
xcleowong <37899969+xcleowong@users.noreply.github.com>
2018-12-05 08:45:16 +0800

    xcleowong

--- AgileNews/AgileNewsEntity/News.cs
+++ AgileNews/AgileNewsEntity/News.cs
@@ -24,6 +24,42 @@ namespace AgileNewsEntity
         /// <summary>
         /// 新聞內容
         /// </summary>
-        public string NewsContent { get; set; }
+        //public string NewsContent { get; set; }
+
+        /// <summary>
+        /// 新闻发布人
+        /// </summary>
+        public string NewsPublisher { get; set; }
+
+        /// <summary>
+        /// 新闻发布时间
+        /// </summary>
+        public DateTime? NewsPublishDate { get; set; }
+
+        /// <summary>
+        /// 新闻细节
+        /// </summary>
+        public string NewsDetails { get; set; }
+
+        /// <summary>
+        /// 新闻排序
+        /// </summary>
+        public int NewsSort { get; set; }
+
+        /// <summary>
+        /// 新闻阅读人数
+        /// </summary>
+        public int NewsReader { get; set; }
+
+        /// <summary>
+        /// 新闻修改时间
+        /// </summary>
+        public DateTime? NewsUpdateDate { get; set; }
+
+        /// <summary>
+        /// 新闻类型外键
+        /// </summary>
+        public string NewsTypeID { get; set; }
+
     }
 }
---
+++ AgileNews/AgileNewsEntity/NewsType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileNewsEntity
+{
+    /// <summary>
+    /// 新闻类别表
+    /// </summary>
+    public class NewsType
+    {
+        /// <summary>
+        /// 新闻类型ID
+        /// </summary>
+        public int NewsTypeID { get; set; }
+        /// <summary>
+        /// 新闻类型名称
+        /// </summary>
+        public string NewsTypeName { get; set; }
+        /// <summary>
+        /// 新闻类型描述
+        /// </summary>
+        public string NewsTypeDesr { get; set; }
+        /// <summary>
+        /// 新闻类型修改时间
+        /// </summary>
+        public DateTime? NewsTypeUpdateDate { get; set; }
+        /// <summary>
+        /// 新闻类型外键PID
+        /// </summary>
+        public int NewsTypePID { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? NewsTypeCreate { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? NewsTypeUpdate { get; set; }
+
+
+
+    }
+}
